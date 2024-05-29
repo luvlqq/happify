0f61e706f096cd3f6efa87e2e2e6786c5d514642
Ivan Lialiuk <lualgod92@gmail.com>
2024-05-29 19:35:25 +0300

    feat: add pg backup script

---
+++ src/scripts/pgsql_backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Backup a Postgresql database into a daily file.
+#
+CONTAINER_BACKUP_DIR=/pg_backup
+PROJECT_BACKUP_DIR=/home/dev/gitfork/happify/backups
+DAYS_TO_KEEP=14
+FILE_SUFFIX=_pg_backup.sql
+DATABASE=fitapp
+USER=postgres
+
+FILE=`date +"%Y%m%d%H%M"`${FILE_SUFFIX}
+CONTAINER_OUTPUT_FILE=${CONTAINER_BACKUP_DIR}/${FILE}
+HOST_OUTPUT_FILE=${PROJECT_BACKUP_DIR}/${FILE}
+
+docker exec 4cfd22e7d5bf mkdir -p ${CONTAINER_BACKUP_DIR}
+
+mkdir -p ${PROJECT_BACKUP_DIR}
+
+docker exec 4cfd22e7d5bf pg_dump -U ${USER} ${DATABASE} -F p -f ${CONTAINER_OUTPUT_FILE}
+
+docker cp 4cfd22e7d5bf:${CONTAINER_OUTPUT_FILE} ${HOST_OUTPUT_FILE}
+
+gzip ${HOST_OUTPUT_FILE}
+
+echo "${HOST_OUTPUT_FILE}.gz was created:"
+ls -l ${HOST_OUTPUT_FILE}.gz
+
+find ${PROJECT_BACKUP_DIR} -maxdepth 1 -mtime +${DAYS_TO_KEEP} -name "*${FILE_SUFFIX}.gz" -exec rm -rf '{}' ';'
